14ab1b0fbdf176ec4f35497af2c1500182fdbb83
IvánG <ivangarciaganado@gmail.com>
2016-02-17 16:43:02 +0100

    js3

--- ejercicios/js3/index.js
+++ ejercicios/js3/index.js
@@ -6,14 +6,20 @@
     - Una propiedad center con un array de dos valores (-3.688510, 40.453010)
     - Una propiedad zoom con valor 16
 */
-
-
+var mapa = {
+  'basemap' : 'topo',
+  'center' : [-3.688510, 40.453010],
+  'zoom' : 16
+};
 /*
   Añádele una propiedad 'autoResize' a verdadero (booleano)
 */
-
-
-
+mapa.autoResize = true;
 /*
   Imprime todos los valores del objeto usando un blucle for
 */
+for(var property in mapa) {    
+    if (mapa.hasOwnProperty(property)) {
+        alert('mapa<' + property + '> = ' + mapa[property]);
+    }
+}
